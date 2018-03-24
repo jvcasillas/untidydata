1f7aef628f4b3ac0cc6c0bf09bc8aca3408a7006
jvcasillas <joseph.casillas@rutgers.edu>
2018-03-24 15:32:57 -0400

    smal changes

--- data-raw/create_spirantization_data.R
+++ data-raw/create_spirantization_data.R
@@ -31,7 +31,7 @@ spirantization <- expand.grid(id = c(sprintf("beg_%02d", 1:n),
   cIntensity = c(rnorm(n = n, mean = 60, sd = 4),  # beg in initial position
                  rnorm(n = n, mean = 60, sd = 4),  # beg in vcv position
                  rnorm(n = n, mean = 60, sd = 4),  # adv in initial position
-                 rnorm(n = n, mean = 60, sd = 4),  # adv in vcv position
+                 rnorm(n = n, mean = 61.5, sd = 4),  # adv in vcv position
                  rnorm(n = n, mean = 59, sd = 4),  # nat in initial position
                  rnorm(n = n, mean = 72, sd = 4)), # nat in vcv position
   vIntensity = c(rnorm(n = n, mean = 79, sd = 4),  # beg in initial position
